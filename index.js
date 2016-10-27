5be2e56f8e7f9fe9e080af92babd1891894c6fab
JSProto <jake.arefyev@gmail.com>
2016-10-27 21:41:02 +0300

    create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+let alphabet = 5; // A,B...E
+let numbers = 12; // 0,1,2...12
+
+let pins = Array(alphabet).fill(0).reduce(function(m, v, a){
+	a = String.fromCharCode(a + 65);
+
+	Array(numbers + 1).fill(0).forEach(function(v, n){
+		try {
+			Function('return ' + a + n)();
+		} catch (e) {
+			return false;
+		}
+		return m.push(a + n);
+	});
+
+	return m;
+}, []);
+
+// DEBUG
+// pins.forEach(console.log);
+
+module.exports = pins;
