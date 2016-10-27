50a1066f35805e81c651c4f3f642f5926b676f5e
JSProto <jake.arefyev@gmail.com>
2016-10-27 21:43:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-let alphabet = 5; // A,B...E
-let numbers = 12; // 0,1,2...12
+let alphabet = 5;
+let numbers = 12;
 
 let pins = Array(alphabet).fill(0).reduce(function(m, v, a){
-	a = String.fromCharCode(a + 65);
+    a = String.fromCharCode(a + 65);
 
-	Array(numbers + 1).fill(0).forEach(function(v, n){
-		try {
-			Function('return ' + a + n)();
-		} catch (e) {
-			return false;
-		}
-		return m.push(a + n);
-	});
+    Array(numbers + 1).fill(0).forEach(function(v, n){
+        try {
+            Function('return ' + a + n)();
+        } catch (e) {
+            return false;
+        }
+        return m.push(a + n);
+    });
 
-	return m;
+    return m;
 }, []);
 
 // DEBUG
